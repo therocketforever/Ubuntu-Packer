6f4fe135bca101be3fd7839a2261460761733a4f
Victor Passapera <vpassapera@gmail.com>
2013-10-07 01:18:21 -0700

    Updates to stabilize

--- scripts/base.sh
+++ scripts/base.sh
@@ -1,6 +1,6 @@
 apt-get -y update
 apt-get -y upgrade
-apt-get -y install gcc build-essential linux-headers-$(uname -r)
+apt-get -y install gcc build-essential linux-headers-$(uname -r) nfs-kernel-server
 apt-get -y install zlib1g-dev libssl-dev libreadline-gplv2-dev libyaml-dev
 apt-get -y install vim curl
 aptitude -y install puppet ssh wget curl
